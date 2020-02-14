530283369d37aba912c3be6fd16298cd8a5412d0
BNK <balank244@gmail.com>
2020-02-14 20:49:25 +0530

    Init commit

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
 
-const publicDirectaryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialspath = path.join(__dirname,'../templates/partials')
+const publicDirectaryPath = path.join(__dirname, './public')
+const viewsPath = path.join(__dirname,'./templates/views')
+const partialspath = path.join(__dirname,'./templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views',viewsPath)
